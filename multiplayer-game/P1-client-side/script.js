189094c777a21280a498c787c1c17e3491c59a17
wiwum <wgward@gmail.com>
2017-05-25 17:22:48 -0500

    add a test line

--- multiplayer-game/P1-client-side/script.js
+++ multiplayer-game/P1-client-side/script.js
@@ -3,5 +3,6 @@ $(document).ready(function(){
         $.get("https://wiwum.github.io/multiplayer-game/P2-client-side/index.asp", function(data, status){
             alert("Data: " + data + "\nStatus: " + status);
         });
+        console.log("working");
     });
 });
--- multiplayer-game/P2-client-side/script.js
+++ multiplayer-game/P2-client-side/script.js
@@ -3,5 +3,6 @@ $(document).ready(function(){
         $.get("https://wiwum.github.io/multiplayer-game/P1-client-side/index.asp", function(data, status){
             alert("Data: " + data + "\nStatus: " + status);
         });
+        console.log("working");
     });
 });
