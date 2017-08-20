4eb990b74ae4c6231b79e07d02f81a1ada9037c5
wiwum <wgward@gmail.com>
2017-08-20 16:06:11 -0500

    added function for changing square color !!! does not work

--- EZ-games/index.js
+++ EZ-games/index.js
@@ -9,9 +9,10 @@ var YLoc = 1;
 // for loop to add the <game-square> elements into the <game-board> element
 for (var i = 0; i < 144; i++) {
   // <game-square> element contents goe down
-  gameBoard.innerHTML =  currentGameBoardData + "<game-square x-loc='" + XLoc + "' y-loc='" + YLoc + "' class='black'></game-square>";
+  gameBoard.innerHTML =  currentGameBoardData + "<game-square location='" + XLoc + "," + YLoc + "'class='black'></game-square>";
   currentGameBoardData = gameBoard.innerHTML;
   XLoc++;
+  // makes a new row to keep the length of each row at twelve.
   if (XLoc == 13) {
     XLoc = 1;
     YLoc++;
@@ -22,6 +23,15 @@ for (var i = 0; i < 144; i++) {
 
 // color change function
 var changeColor = function (x, y, c) {
-  var selectedSquare = document.querySelector('[x-loc = "x"][y-loc = y]');
-  selectedSquare.backgroundColor = c;
+  var squareCoordinates = x + "," + y; // puts the squares coordinates into a string.
+  console.log(squareCoordinates);
+  var selectedSquare = document.querySelector('[location=squareCoordinates]'); // finds square with the coordinates
+  //sends error message if square can't be found
+  if (selectedSquare == undefined) {
+    console.log("could not find selected square");
+  }
+  // changes class of square in order to change its color (see class-styles.css)
+  if (selectedSquare != undefined) {
+    selectedSquare.class = c;
+  }
 };
